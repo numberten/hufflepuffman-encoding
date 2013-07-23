2782fff8c0bb6db0bfb3abc60e0ade74387aa07d
jesse <number.dot.ten@gmail.com>
2013-07-22 23:18:30 -0700

    Added main functions for compiled speed comparisons. Refactored version running 22 times faster, with both files compiled using -O2 compiler optimizations.

--- Huffman.hs
+++ Huffman.hs
@@ -1,4 +1,10 @@
 import Huffman.Encode
 import Huffman.Decode
 
+main = do
+        putStrLn ("Length of string being encoded: " ++ (show $ length s))
+        putStrLn . uncurry decode $ encode s
+    where
+        s = concat $ replicate 100 "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
 
--- huffman.hs
+++ huffman.hs
@@ -5,6 +5,11 @@ data BTree a = Leaf a | Node (BTree a) (BTree a) deriving (Eq, Show)
 type Tup a = (BTree a, Int)
 type PQueue a = [Tup a]
 
+main = do
+        putStrLn . frombinary $ tobinary s 
+    where
+        s = concat $ replicate 100 "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
 add :: Tup a -> PQueue a -> PQueue a
 add a b = add_w a [] b
    where
